c956c35110aa95803f5340e4da4b503f6e12b26b
Miki Sh <srnin23@gmail.com>
2024-08-25 17:07:52 +0900

    ✅ Todoモデルに存在性と長さのバリデーションを追加し、そのテストを作成

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,3 @@
 class Todo < ApplicationRecord
+  validates :task, presence: true, length: { maximum: 30 }
 end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @todo = Todo.new(task: "火の元を確認する")
+  end
+
+  test "有効なTodoか" do
+    assert @todo.valid?
+  end
+
+  test "taskが空欄だと無効になるか" do
+    @todo.task = "   "
+    assert_not @todo.valid?
+  end
+
+  test "taskが31文字以上だと無効になるか" do
+    @todo.task = "a" * 31
+    assert_not @todo.valid?
+  end
 end
