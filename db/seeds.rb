9dcbdb01cad4a0b4e8a6ca0d738b527c33ce6118
Miki Sh <srnin23@gmail.com>
2024-08-25 17:29:00 +0900

    🌱 Todoモデルのseedファイルを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# このファイルは、アプリケーションをすべての環境（本番、開発、テスト）で動作させるために必要なレコードの存在を確保するためのものです。
+# ここに記述されるコードは冪等性（べきとうせい）を持つべきであり、どの環境でも任意のタイミングで実行できるようにする必要があります。
+# このデータは、bin/rails db:seed コマンドでロードされるか、db:setupとともにデータベースに作成されます。
 #
-# Example:
+# 例:
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+json = ActiveSupport::JSON.decode(File.read('db/seeds/todos.json'))
+json.each do |record|
+  Todo.create!(record)
+end
