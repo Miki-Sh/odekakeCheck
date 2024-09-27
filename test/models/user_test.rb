aab9992bd1e1d59008c73bc8b41ace7f163c07ce
Miki Sh <srnin23@gmail.com>
2024-09-27 16:10:45 +0000

    🦺 emailの形式のバリデーションを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,4 +19,22 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "emailのバリデーションは有効なアドレスを受け入れる" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect}は有効なメールアドレスです"
+    end
+  end
+
+  test "emailのバリデーションは無効なアドレスを受け入れない" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect}は無効なメールアドレスです"
+    end
+  end
 end
