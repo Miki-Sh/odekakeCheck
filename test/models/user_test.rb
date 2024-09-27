1dd486a6d0bf495df868c8abd357e3d658c4c64a
Miki Sh <srnin23@gmail.com>
2024-09-27 15:06:14 +0000

    ✅ 🦺 emailのバリデーションとそのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "空欄のemailは無効" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "emailは255文字以下" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
