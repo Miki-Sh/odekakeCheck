3dba14925e3c7cf7626d6580edc551ba6673650c
Miki Sh <srnin23@gmail.com>
2024-09-27 14:47:59 +0000

    🗃️ usersテーブルの不要なカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_27_093941) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
