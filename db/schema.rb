46c73cdded6b5ba382d1753b19e5653e8e4b5b22
Miki Sh <srnin23@gmail.com>
2024-09-27 09:47:58 +0000

    🗃️ Userモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_28_172903) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_27_093941) do
   create_table "todos", force: :cascade do |t|
     t.string "task", null: false
     t.boolean "is_completed", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
