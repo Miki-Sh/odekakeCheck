21ecf9d534cf6489ec9cddf4312526db23daf824
Miki Sh <srnin23@gmail.com>
2024-09-29 14:06:31 +0000

    ✅ ルーティングの変更とテストの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
-  # サイトのインデックスページにアクセスするルート
-  # get 'site/index'
-
-  # ルーティングDSL（Domain-Specific Language）に従ってアプリケーションのルートを定義します。
-  # 詳しくは https://guides.rubyonrails.org/routing.html を参照してください。
-
   # /up にヘルスチェックのルートを公開し、アプリが例外なく起動している場合は200を返し、それ以外の場合は500を返します。
   # ロードバランサーやアップタイムモニターがアプリの稼働状況を確認するために使用します。
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root to: redirect('/todos')
-
-  get 'todos', to: 'site#index'
-  get 'todos/new', to: 'site#index'
-  get 'todos/:id', to: 'site#index'
-  get 'todos/:id/edit', to: 'site#index'
-  get  'todos/signup',  to: "users#new"
+  root "site#index"
+  get "/new", to: "site#index"
+  get "/:id", to: "site#index"
+  get "/:id/edit", to: "site#index"
+  get "/signup",  to: "users#new"
+  resources :users
 
   namespace :api do
     resources :todos, only: %i[index show create destroy update]
--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class SiteControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get site_index_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get root_path
+    assert_response :success
+  end
 end
