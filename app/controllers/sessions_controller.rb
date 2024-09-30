3069fefcbb125ef35ce7b987b2aeb4f067dfd2c3
Miki Sh <srnin23@gmail.com>
2024-09-30 08:12:19 +0000

    ✨ ログイン機能作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      reset_session      # セッション固定攻撃への対策（ログインの直前に必ずこれを書くこと）
+      log_in @user
+    else
+      render json: { errors: "メールアドレスとパスワードの組み合わせが間違っています" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # /up にヘルスチェックのルートを公開し、アプリが例外なく起動している場合は200を返し、それ以外の場合は500を返します。
   # ロードバランサーやアップタイムモニターがアプリの稼働状況を確認するために使用します。
   get "up" => "rails/health#show", as: :rails_health_check
@@ -8,6 +9,9 @@ Rails.application.routes.draw do
   get "/:id", to: "site#index"
   get "/:id/edit", to: "site#index"
   get "/signup",  to: "users#new"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
   resources :users
 
   namespace :api do
