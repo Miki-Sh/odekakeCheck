7f36bfab491726ef0539622ba9826c1656499187
Miki Sh <srnin23@gmail.com>
2024-09-30 08:48:56 +0000

    ✨ ログアウト機能作成

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,13 +7,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "無効なemailでログインする" do
     get login_path
-    post login_path, params: { session: { email: "", password: @user.password } }
+    post login_path, params: { session: { email: "hoge@email.com", password: "password" } }
     assert_response :unprocessable_entity
   end
 
   test "無効なpasswordでログインする" do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: "" } }
+    post login_path, params: { session: { email: @user.email, password: "hogehoge" } }
     assert_response :unprocessable_entity
+  end
+
+  test "有効な情報でログインして、ログアウトする" do
+    post login_path, params: { session: { email: @user.email, password: "password" } }
+    assert is_logged_in?
+    delete logout_path
+    assert_not is_logged_in?
+    assert_response :see_other
   end
 end
