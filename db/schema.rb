e6acda4059b2c8a9b30612e9a422f66ebf544136
Miki Sh <srnin23@gmail.com>
2024-09-30 08:57:10 +0000

    🗃️ usersテーブルにremember_digestカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,10 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
+# このファイルは、データベースの現在の状態から自動生成されています。このファイルを編集するのではなく、Active Record のマイグレーション機能を使用してデータベースを段階的に修正し、その後このスキーマ定義を再生成してください。
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_28_144022) do
+# このファイルは、bin/rails db:schema:loadを実行するときに Rails がスキーマを定義するためのソースです。新しいデータベースを作成する際、bin/rails db:schema:loadは通常、すべてのマイグレーションを最初から実行するよりも速く、エラーが発生する可能性が低いです。古いマイグレーションは、外部依存関係やアプリケーションコードを使用している場合、正しく適用されない可能性があります。
+
+# このファイルは、バージョン管理システムにチェックインすることを強く推奨します。
+
+ActiveRecord::Schema[7.1].define(version: 2024_09_30_085153) do
   create_table "todos", force: :cascade do |t|
     t.string "task", null: false
     t.boolean "is_completed", default: false, null: false
@@ -23,6 +17,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_28_144022) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
