d5015767f9bb0fc3bd84e576c5f7986a9e4c35da
Miki Sh <srnin23@gmail.com>
2024-10-06 08:25:57 +0000

    🚀

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module OdekakeCheck
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    onfig.middleware.delete ActionDispatch::HostAuthorization
   end
 end
